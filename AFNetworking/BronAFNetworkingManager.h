8a5b7de101866c74db469b6301815b949c1bdf48
hbj520 <hbjmailformac@163.com>
2018-11-08 20:35:23 +0800

    添加测试--han

---
+++ AFNetworking/BronAFNetworkingManager.h
@@ -0,0 +1,17 @@
+//
+//  BronAFNetworkingManager.h
+//  AFNetworking iOS
+//
+//  Created by Bron on 2018/11/8.
+//  Copyright © 2018 AFNetworking. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BronAFNetworkingManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AFNetworking/BronAFNetworkingManager.m
@@ -0,0 +1,13 @@
+//
+//  BronAFNetworkingManager.m
+//  AFNetworking iOS
+//
+//  Created by Bron on 2018/11/8.
+//  Copyright © 2018 AFNetworking. All rights reserved.
+//   afnetworking
+
+#import "BronAFNetworkingManager.h"
+
+@implementation BronAFNetworkingManager
+
+@end
